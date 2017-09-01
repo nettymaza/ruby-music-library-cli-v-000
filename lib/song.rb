8ab2d70e9fd79d3e98a0caec77f08c656ea6ca32
Annette Maza <nettymaza@gmail.com>
2017-09-01 20:27:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,7 +51,10 @@ class Song
     # end
   end
 
-  def self.new_from_filename
+  def self.new_by_filename(filename)
+    song = self.new(filename.split(" - ")[1])
+    song.artist = Artist.find_or_create_by_name(filename.split(" - ")[0])
+    song
   end
 
 
