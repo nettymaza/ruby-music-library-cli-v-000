d41dd5377107b7d5be7f17a8fba9eaa782d675e7
Annette Maza <nettymaza@gmail.com>
2017-09-01 21:56:45 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -41,7 +41,7 @@ class MusicLibraryController
 def list_songs
   Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |song, index|
     puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
-    binding.pry
+    # binding.pry
   end
 end
 
