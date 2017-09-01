aac384690cf6152e990de5cb3d2870c1ec835310
Annette Maza <nettymaza@gmail.com>
2017-09-01 21:50:56 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -39,8 +39,8 @@ class MusicLibraryController
   end
 
 def list_songs
-  Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
-    puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+  Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |song, index|
+    puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     binding.pry
   end
 end
