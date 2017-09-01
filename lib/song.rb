423679fc5f9e660335483ef17f5000c714537856
Annette Maza <nettymaza@gmail.com>
2017-09-01 20:45:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -58,15 +58,16 @@ class Song
     song.artist = Artist.find_or_create_by_name(filename.split(" - ")[0])
     song.genre = Genre.find_or_create_by_name(filename.split(" - ")[2].gsub(".mp3", "")
     song
+    binding.pry 
   end
 
   # def self.new_from_filename(filename)
   #  parts = filename.split(" - ")
   #  artist_name, song_name, genre_name = parts[0], parts[1], parts[2].gsub(".mp3", "")
-  # 
+  #
   #  artist = Artist.find_or_create_by_name(artist_name)
   #  genre = Genre.find_or_create_by_name(genre_name)
-  # 
+  #
   #  new(song_name, artist, genre)
  end
 
